b70a7eeb60109209292f35d8b2576984b17cd1ad
Amarnath Karthi <97amarnathk@gmail.com>
2019-03-15 01:17:43 +0530

    Add  header to handle HDF5 operations

--- read.cpp
+++ read.cpp
@@ -1,28 +1,28 @@
+/***************************************************************************************
+* Author : Amarnath Karthi
+* read.cpp
+* 
+* Example of reading 100 SimpleStruct Objects from a file using `SimpleStructH5Wrapper.h`
+****************************************************************************************/
 #include <iostream>
 #include <vector>
-#include "H5Cpp.h"
+#include <array>
 
 #include "SimpleStruct.h"
+#include "SimpleStructH5Wrapper.h"
 
 using namespace std;
 using namespace H5;
 
 const string FILE_NAME( "SimpleStructFile.h5" );
 const string DATASET_NAME( "ArraySimpleStruct" );
-const string MEMBER_X = "x";
-const string MEMBER_Y = "y";
-const string MEMBER_Z = "z";
-const string MEMBER_P = "p";
 
 const int num_elements = 100;
-const int RANK = 1;
-const int ARRAY_RANK = 1;
-const hsize_t asize = 4;
 
-
-bool verify(SimpleStruct *data, int num) {
-    for(int i=0; i<num; i++) {
-        for(int j=0; j<4; j++) {
+template<std::size_t SIZE>
+bool verify(array<SimpleStruct, SIZE> data) {
+    for(int i=0; i<data.size(); i++) {
+        for(int j=0; j<data[i].p.size(); j++) {
             if(data[i].p[j]!=i)
                 return false;
         }
@@ -34,37 +34,12 @@ bool verify(SimpleStruct *data, int num) {
 }
 
 int main() {
-    
-    SimpleStruct data[num_elements];
-
-    H5File *file = new H5File(FILE_NAME, H5F_ACC_RDONLY);
-    DataSet *dataset = new DataSet (file->openDataSet( DATASET_NAME ));
-
-    /*
-    * Create the memory datatype.
-    */
-    ArrayType array_type(PredType::NATIVE_INT, ARRAY_RANK,&asize);
-    CompType mtype1( sizeof(SimpleStruct) );
-    mtype1.insertMember( MEMBER_P, HOFFSET(SimpleStruct, p), array_type);
-    mtype1.insertMember( MEMBER_X, HOFFSET(SimpleStruct, x), PredType::NATIVE_INT);
-    mtype1.insertMember( MEMBER_Y, HOFFSET(SimpleStruct, y), PredType::NATIVE_INT);
-    mtype1.insertMember( MEMBER_Z, HOFFSET(SimpleStruct, z), PredType::NATIVE_INT);
-
-    /*
-    * Load content
-    */
-    dataset->read( data, mtype1 );
+    array<SimpleStruct, num_elements> data;
+    readSimpleStructArray(data, FILE_NAME, DATASET_NAME);
 
     cout<<"Checking data...."<<endl;
-    if(verify(data, num_elements))
+    if(verify(data))
         cout<<"SUCCESS"<<endl;
     else
         cout<<"FAILURE"<<endl;
-    
-    /*
-    * Release resources
-    */
-    delete dataset;
-    delete file;
-    return 0; 
 }
--- write.cpp
+++ write.cpp
@@ -1,32 +1,27 @@
+/***************************************************************************************
+* Author : Amarnath Karthi
+* read.cpp
+* 
+* Example of writing 100 SimpleStruct Objects to a file using `SimpleStructH5Wrapper.h`
+****************************************************************************************/
+
 #include <iostream>
 #include <vector>
-#include <H5Cpp.h>
-#include <hdf5.h>
+#include <array>
 
 #include "SimpleStruct.h"
+#include "SimpleStructH5Wrapper.h"
 
 using namespace std;
 using namespace H5;
 
 const string FILE_NAME( "SimpleStructFile.h5" );
 const string DATASET_NAME( "ArraySimpleStruct" );
-const string MEMBER_X = "x";
-const string MEMBER_Y = "y";
-const string MEMBER_Z = "z";
-const string MEMBER_P = "p";
 
 const int num_elements = 100;
-const int RANK = 1;
-const int ARRAY_RANK = 1;
-const hsize_t asize = 4;
 
 int main() {
-    int num_elements = 100;
-    int RANK = 1;
-    int ARRAY_RANK = 1;
-    hsize_t asize = 4;
-
-    SimpleStruct data[num_elements];
+    array<SimpleStruct, num_elements> data;
 
     /*
     *   Initialize data
@@ -42,47 +37,7 @@ int main() {
         data[i].z = 10*i + 2;
     }
 
-    /*
-    * Create the data space.
-    */
-    hsize_t dim[] = {num_elements};
-    DataSpace space(RANK, dim);
+    writeSimpleStructArray(data, FILE_NAME, DATASET_NAME);
 
-    /*
-    * Create file
-    */
-    cout<<"Creating file "<<FILE_NAME<<"..."<<endl;
-    H5File *file = new H5File(FILE_NAME, H5F_ACC_TRUNC);
-
-    /*
-    * Create the memory datatype.
-    */
-    ArrayType array_type(PredType::NATIVE_INT, ARRAY_RANK,&asize);
-    CompType mtype1( sizeof(SimpleStruct) );
-    mtype1.insertMember( MEMBER_P, HOFFSET(SimpleStruct, p), array_type);
-    mtype1.insertMember( MEMBER_X, HOFFSET(SimpleStruct, x), PredType::NATIVE_INT);
-    mtype1.insertMember( MEMBER_Y, HOFFSET(SimpleStruct, y), PredType::NATIVE_INT);
-    mtype1.insertMember( MEMBER_Z, HOFFSET(SimpleStruct, z), PredType::NATIVE_INT);
-
-
-    /*
-    * Create the dataset.
-    */
-    DataSet* dataset;
-    dataset = new DataSet(file->createDataSet(DATASET_NAME, mtype1, space));
-
-    /*
-    * Write data to the dataset;
-    */
-    cout<<"Writing data..."<<endl;
-    dataset->write( data, mtype1 );
-
-    cout<<"SUCCESS"<<endl;
-
-    /*
-    * Release resources
-    */
-    delete dataset;
-    delete file;
     return 0; 
 }
